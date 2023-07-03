f6d7478c52d9b74a4d8877eeea0b73635a970210
Kirill <Khlebnikov-2016@mail.ru>
2023-07-03 17:35:41 +1100

    Add existing file

---
+++ js/app.js
@@ -0,0 +1,13 @@
+new Swiper(".slider", {
+	direction: 'vertical',
+	speed: 2400,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.4,
+	},
+	spaceBetween: 18,
+	parallax: true,
+	// freeMode: true,
+
+
+})
